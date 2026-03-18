88be661901c42f808f626b47f6a01dac6b76885d
GEOVANE MERÇON SANTOS <25203771@CFP127ALUNO.LOCAL>
2026-03-18 11:27:04 -0300

    Projeto Bem Legal

---
+++ funcoes.js
@@ -0,0 +1,26 @@
+/********************************************************************************************************************************************************************
+ * 
+ * Autor: Geovane
+ * Objetivo: Criar funções 
+ * Data: 18/03/2026
+ * Versão: 1.0
+ * 
+ ********************************************************************************************************************************************************************/
+
+const listaDeEstados = require("./estados_cidades");
+
+
+
+//console.log(listaDeEstados)
+
+const getListaDeEstados = function(){
+
+    let lista = listaDeEstados.estados
+
+    lista.forEach(function(estado){
+
+    })
+}
+
+getListaDeEstados()
+
